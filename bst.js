f70a04ea092d54906adc1a65ed48bf2ef76b2f06
Dylan Simon <grvdylans@gmail.com>
2025-06-12 01:03:30 -0600

    Add base logic for left and right split

--- bst.js
+++ bst.js
@@ -12,7 +12,31 @@ class Tree {
     }
 
     buildTree(array) {
-        
-    }
+        let start = 0;
+        let end = array.length - 1;
+        let middle = start + Math.floor((end - start) / 2);
 
+        // base case
+        if (start > end) {
+            return null;
+        }
+        console.log(start);
+        console.log(end);
+        console.log(middle);
+        // left side
+        if (middle - 1 !== null) {
+            const leftArray = array.slice(start, middle);
+            console.log(leftArray);
+            // this.buildTree();
+        }
+        console.log(array[middle]);
+        if (middle + 1 !== null) {
+            const rightArray = array.slice(middle + 1, end + 1);
+            console.log(rightArray);
+        }
+
+        // right side
+    }
 }
+
+module.exports = { Node, Tree };
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+const { Node, Tree } = require("./bst.js");
+
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 array.sort((a, b) => a - b);
 console.log(array);
+
+let tree = new Tree();
+tree.buildTree(array);
