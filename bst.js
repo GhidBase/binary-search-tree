1e23c1bbc055fa9d9c7633a18e896aea976e22e3
Dylan Simon <grvdylans@gmail.com>
2025-06-12 00:34:28 -0600

    Initial commit - start project

---
+++ bst.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(data = null, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Tree {
+    constructor(root = null) {
+        this.root = root;
+    }
+
+    buildTree(array) {
+        
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+array.sort((a, b) => a - b);
+console.log(array);
